a8e1bf04d9724e323c93864eb3821fbf82ccf717
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-08 16:52:47 +0100

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -2,5 +2,4 @@
 exports.run = (Discord, client, message, args) => {  
 if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("No can do pal!"); 
 client.user.setActivity(args[0], { type: args[1] });
-client.user.setStatus("args[2]");
 }
